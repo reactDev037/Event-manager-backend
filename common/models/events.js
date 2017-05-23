17480d92cb0482a20cd15d0537d7e1ad9e075bc8
reactDev037 <developerit037@gmail.com>
2017-05-23 12:43:20 +0200

    fixed events, added contactsByCode and registeredByCode

--- common/models/events.js
+++ common/models/events.js
@@ -27,7 +27,7 @@ module.exports = function(Events) {
     );
 
      /**
-   * Search by eventCode and return unique record
+   * Search by eventCode and return all contacts
    *
    * @param {String} code
    * @callback {Function} callback
@@ -36,7 +36,7 @@ module.exports = function(Events) {
         //console.log(code);
         Events.find({include: 'contacts', where: {eventCode: code}}, function(err,record){
             if (err) cb(err);
-            if (!err) cb(null, record);
+            if (!err) cb(null, record[0]);
         });
     };
 
@@ -47,6 +47,30 @@ module.exports = function(Events) {
             accepts: {arg: 'code', type: 'string', required: true, http: { source: 'path' }},
             returns: {root: true, type: 'object'},
             description: 'Get list of contacts from event code'
+        }
+    );
+
+     /**
+   * Search by eventCode and return all contacts
+   *
+   * @param {String} code
+   * @callback {Function} callback
+   */
+    Events.registeredByCode = function(code, cb){
+        //console.log(code);
+        Events.find({include: 'registered', where: {eventCode: code}}, function(err,record){
+            if (err) cb(err);
+            if (!err) cb(null, record[0]);
+        });
+    };
+
+    Events.remoteMethod(
+        'registeredByCode',
+        {
+            http: {path: '/registeredByCode/:code', verb: 'get'},
+            accepts: {arg: 'code', type: 'string', required: true, http: { source: 'path' }},
+            returns: {root: true, type: 'object'},
+            description: 'Get list of registered from event code'
         }
     );
 
