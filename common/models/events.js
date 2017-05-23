d9cce0e4a1707980f0a9b38a56a026e49b273109
reactDev037 <developerit037@gmail.com>
2017-05-23 12:36:34 +0200

    fixed events

--- common/models/events.js
+++ common/models/events.js
@@ -12,7 +12,7 @@ module.exports = function(Events) {
         //console.log(code);
         Events.find({where: {eventCode: code}}, function(err,record){
             if (err) cb(err);
-            if (!err) cb(null, record);
+            if (!err) cb(null, record[0]);
         });
     };
 
@@ -22,7 +22,31 @@ module.exports = function(Events) {
             http: {path: '/findByCode/:code', verb: 'get'},
             accepts: {arg: 'code', type: 'string', required: true, http: { source: 'path' }},
             returns: {root: true, type: 'object'},
-            description: 'Get an event from code'
+            description: 'Get an event from event code'
+        }
+    );
+
+     /**
+   * Search by eventCode and return unique record
+   *
+   * @param {String} code
+   * @callback {Function} callback
+   */
+    Events.contactsByCode = function(code, cb){
+        //console.log(code);
+        Events.find({include: 'contacts', where: {eventCode: code}}, function(err,record){
+            if (err) cb(err);
+            if (!err) cb(null, record);
+        });
+    };
+
+    Events.remoteMethod(
+        'contactsByCode',
+        {
+            http: {path: '/contactsByCode/:code', verb: 'get'},
+            accepts: {arg: 'code', type: 'string', required: true, http: { source: 'path' }},
+            returns: {root: true, type: 'object'},
+            description: 'Get list of contacts from event code'
         }
     );
 
