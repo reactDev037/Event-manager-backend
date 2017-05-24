62d54db970a9534cd8b1a64cab7d922a38b6cfaf
reactDev037 <developerit037@gmail.com>
2017-05-24 09:06:03 +0200

    events: aggiunta funzione contactEmailExists: verifica se esiste un contatto a partire da codice evento e indirizzo mail

--- common/models/events.js
+++ common/models/events.js
@@ -74,4 +74,40 @@ module.exports = function(Events) {
         }
     );
 
+    /**
+   * Search by eventCode and email and return 1 contact
+   *
+   * @param {String} code
+   * @param {String} mail
+   * @callback {Function} callback
+   */
+    Events.contactEmailExists = function(code, mail, cb){
+        Events.find({
+            where: {eventCode: code}, 
+            include: {
+                relation: 'contacts', 
+                scope: {
+                    where: {email: mail}
+                }
+            }
+        },
+        function(err,record){
+            if (err) cb(err);
+            if (!err) cb(null, record[0]);
+        });
+    };
+
+    Events.remoteMethod(
+        'contactEmailExists',
+        {
+            http: {path: '/contactEmailExists/:code/:mail', verb: 'get'},
+            accepts: [
+                {arg: 'code', type: 'string', required: true, http: { source: 'path' }},
+                {arg: 'mail', type: 'string', required: true, http: { source: 'path' }}
+            ],
+            returns: {root: true, type: 'object'},
+            description: 'Get 1 contact if from event code and with matched email'
+        }
+    );
+
 };
